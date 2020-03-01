5e0b16e2eda8cd55ba9c61d6972593b0b58ef500
Eitan Avgil <eitan.avgil@kaltura.com>
2020-03-01 10:15:57 +0200

    feat(FEV-468): pause and resume video logic

--- src/moderation-plugin.tsx
+++ src/moderation-plugin.tsx
@@ -1,12 +1,12 @@
 import {h} from 'preact';
 import {
-  ContribPluginManager,
-  CorePlugin,
-  OnMediaLoad,
-  OnMediaUnload,
-  ContribServices,
-  ContribPluginData,
-  ContribPluginConfigs,
+    ContribPluginManager,
+    CorePlugin,
+    OnMediaLoad,
+    OnMediaUnload,
+    ContribServices,
+    ContribPluginData,
+    ContribPluginConfigs,
 } from '@playkit-js-contrib/plugin';
 import {UpperBarItem, OverlayItem, OverlayPositions} from '@playkit-js-contrib/ui';
 import {getContribLogger, ObjectUtils} from '@playkit-js-contrib/common';
@@ -17,79 +17,98 @@ import {PluginButton} from './components/plugin-button';
 const pluginName = `moderation`;
 
 const logger = getContribLogger({
-  class: 'ModerationPlugin',
-  module: 'moderation-plugin',
+    class: 'ModerationPlugin',
+    module: 'moderation-plugin',
 });
 
 const {get} = ObjectUtils;
 
-interface ModerationPluginConfig {}
+interface ModerationPluginConfig {
+}
 
 export class ModerationPlugin
-  implements OnMediaLoad, OnMediaUnload, OnMediaUnload {
-  private _upperBarItem: UpperBarItem | null = null;
-  private _moderationOverlay: OverlayItem | null = null;
+    implements OnMediaLoad, OnMediaUnload, OnMediaUnload {
+    private _upperBarItem: UpperBarItem | null = null;
+    private _moderationOverlay: OverlayItem | null = null;
+    private _wasPlayed = false; // keep state of the player so we can resume if needed
 
-  constructor(
-    private _corePlugin: CorePlugin,
-    private _contribServices: ContribServices,
-    private _configs: ContribPluginConfigs<ModerationPluginConfig>
-  ) {}
+    constructor(
+        private _corePlugin: CorePlugin,
+        private _contribServices: ContribServices,
+        private _configs: ContribPluginConfigs<ModerationPluginConfig>,
+        private _player: KalturaPlayerTypes.Player
+    ) {}
 
-  onMediaLoad(): void {
-    logger.trace('Info plugin loaded', {
-      method: 'onMediaLoad',
-    });
-    this._addPluginIcon();
-  }
-
-  onMediaUnload(): void {
-    if (this._upperBarItem) {
-      this._contribServices.upperBarManager.remove(this._upperBarItem);
-      this._upperBarItem = null;
+    onMediaLoad(): void {
+        logger.trace('Info plugin loaded', {
+            method: 'onMediaLoad',
+        });
+        this._addPluginIcon();
     }
-    if (this._moderationOverlay) {
-      this._toggleOverlay();
-    }
-  }
 
-  private _toggleOverlay = () => {
-    if (this._moderationOverlay) {
-      this._contribServices.overlayManager.remove(this._moderationOverlay);
-      this._moderationOverlay = null;
-      return;
+    onMediaUnload(): void {
+        if (this._upperBarItem) {
+            this._contribServices.upperBarManager.remove(this._upperBarItem);
+            this._upperBarItem = null;
+        }
+        if (this._moderationOverlay) {
+            this._toggleOverlay();
+        }
     }
-    this._moderationOverlay = this._contribServices.overlayManager.add({
-      label: 'moderation-overlay',
-      position: OverlayPositions.PlayerArea,
-      renderContent: () => (
-        <Moderation
-          onClick={this._toggleOverlay}
-        />
-      ),
-    });
-  };
 
-  private _addPluginIcon(): void {
-    const {} = this._configs.pluginConfig;
-    this._upperBarItem = this._contribServices.upperBarManager.add({
-      label: 'Moderation',
-      onClick: this._toggleOverlay,
-      renderItem: () => <PluginButton />,
-    });
-  }
+    private _toggleOverlay = () => {
+        const isPlaying = !(this._player as any).paused;
+        logger.trace(`Info toggle overlay player`, {
+            method: '_toggleOverlay'
+        });
+        if (this._moderationOverlay) {
+            this._contribServices.overlayManager.remove(this._moderationOverlay);
+            this._moderationOverlay = null;
+            if (this._wasPlayed) {
+                logger.trace(`Info plugin paused player`, {
+                    method: '_toggleOverlay'
+                });
+                this._wasPlayed = false;
+                this._player.play();
+            }
+            return;
+        }
+        if (isPlaying) {
+            this._wasPlayed = true;
+            this._player.pause();
+        }
+        this._moderationOverlay = this._contribServices.overlayManager.add({
+            label: 'moderation-overlay',
+            position: OverlayPositions.PlayerArea,
+            renderContent: () => (
+                <Moderation
+                    onClick={this._toggleOverlay}
+                />
+            ),
+        });
+    };
+
+    private _addPluginIcon(): void {
+        const {} = this._configs.pluginConfig;
+        this._upperBarItem = this._contribServices.upperBarManager.add({
+            label: 'Moderation',
+            onClick: this._toggleOverlay,
+            renderItem: () => <PluginButton/>,
+        });
+    }
 }
 
 ContribPluginManager.registerPlugin(
-  pluginName,
-  (data: ContribPluginData<ModerationPluginConfig>) => {
-    return new ModerationPlugin(
-      data.corePlugin,
-      data.contribServices,
-      data.configs
-    );
-  },
-  {
-    defaultConfig: {},
-  }
+    pluginName,
+    (data: ContribPluginData<ModerationPluginConfig>) => {
+        return new ModerationPlugin(
+            data.corePlugin,
+            data.contribServices,
+            data.configs,
+            data.player
+        );
+    },
+    {
+        defaultConfig: {},
+    }
 );
