a96973fe9ac830370c0e86ab5d324fecd59798de
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2020-04-14 18:35:28 +0300

    chore(ui): moderation icon classname fix (FEV-530)

--- src/components/plugin-button/plugin-button.tsx
+++ src/components/plugin-button/plugin-button.tsx
@@ -2,5 +2,5 @@ const {h} = KalturaPlayer.ui.preact;
 import * as styles from './plugin-button.scss';
 
 export const PluginButton = () => (
-  <button className={styles.pluginButton} tabIndex={1} />
+  <button className={styles.moderationIcon} tabIndex={1} />
 );
