344a58c8f69e1e220e1c4e7b0fe7d7b94c53e1d2
JonathanTGold <jonathan.gold@86@gmail.com>
2021-11-25 11:54:40 +0200

    replace with Fradment

--- src/components/moderation/moderation.tsx
+++ src/components/moderation/moderation.tsx
@@ -1,4 +1,4 @@
-import {h, Component} from 'preact';
+import {h, Component, Fragment} from 'preact';
 import {getContribLogger, ObjectUtils} from '@playkit-js-contrib/common';
 import {
   KeyboardKeys,
@@ -190,7 +190,7 @@ export class Moderation extends Component<ModerationProps, ModerationState> {
             verticalPosition={PopoverVerticalPositions.Bottom}
             horizontalPosition={PopoverHorizontalPositions.Right}
             content={this._popoverContent()}>
-            <div>
+            <Fragment>
               <button className={styles.selectWrapper} tabIndex={1}>
                 <div className={styles.select}>
                   {reportContentType > -1
@@ -199,7 +199,7 @@ export class Moderation extends Component<ModerationProps, ModerationState> {
                 </div>
                 <div className={styles.downArrow} />
               </button>
-            </div>
+            </Fragment>
           </Popover>
           <form onSubmit={this._handleSubmit}>
             <textarea
