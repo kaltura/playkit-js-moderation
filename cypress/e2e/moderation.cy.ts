fbca84bc718c19938aefc8acc97f51b7b8ed21bc
Serhii <serhii.marchenko@Kaltura.com>
2023-05-10 21:39:28 +0300

    fix(FEC-13145): upgrade dependancies for common and ui-managers etc (#54)
    * fix(FEC-13145): upd dependencies and tests scripts
    
    * fix(FEC-13145): upd tests scripts

--- cypress/e2e/moderation.cy.ts
+++ cypress/e2e/moderation.cy.ts
@@ -1,28 +1,35 @@
 import {
   addComment,
   closeModerationPlugin,
-  openModerationPlugin, openPopoverMenu,
+  openModerationPlugin,
+  openPopoverMenu,
   reportAndVerifyRequest,
-  openAndSelectItemFromDropdown
-} from "./utils";
+  openAndSelectItemFromDropdown,
+  loadPlayer,
+  mockKalturaBe
+} from './utils';
 
 const MANIFEST = `#EXTM3U
 #EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio",LANGUAGE="en",NAME="English",AUTOSELECT=YES,DEFAULT=YES,URI="${location.origin}/media/index_1.m3u8",SUBTITLES="subs"
-
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=509496,RESOLUTION=480x272,AUDIO="audio",SUBTITLES="subs"
 ${location.origin}/media/index.m3u8`;
 
+const MANIFEST_SAFARI = `#EXTM3U
+#EXT-X-MEDIA:TYPE=SUBTITLES,GROUP-ID="subs",NAME="English",DEFAULT=NO,AUTOSELECT=YES,FORCED=NO,LANGUAGE="en",URI="${location.origin}/media/index_1.m3u8"
+#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=504265,RESOLUTION=480x272,AUDIO="audio",SUBTITLES="subs"
+${location.origin}/media/index.m3u8`;
+
 const moderateItems = [
-    {id: 1, label: 'Sexual Content'},
-    {id: 2, label: 'Violent Or Repulsive'},
-    {id: 3, label: 'Harmful Or Dangerous Act'},
-    {id: 4, label: 'Spam / Commercials'}
+  {id: 1, label: 'Sexual Content'},
+  {id: 2, label: 'Violent Or Repulsive'},
+  {id: 3, label: 'Harmful Or Dangerous Act'},
+  {id: 4, label: 'Spam / Commercials'}
 ];
 
 describe('Moderation plugin', () => {
   beforeEach(() => {
     // manifest
-    cy.intercept('GET', '**/a.m3u8*', MANIFEST);
+    cy.intercept('GET', '**/a.m3u8*', Cypress.browser.name === 'webkit' ? MANIFEST_SAFARI : MANIFEST);
     // thumbnails
     cy.intercept('GET', '**/width/164/vid_slices/100', {fixture: '100.jpeg'});
     cy.intercept('GET', '**/height/360/width/640', {fixture: '640.jpeg'});
@@ -30,56 +37,73 @@ describe('Moderation plugin', () => {
     cy.intercept('GET', '**/index.php?service=analytics*', {});
     // go to test page
     cy.intercept('POST', 'http://mock-api/service/multirequest', {fixture: 'vod-entry.json'});
-    cy.visit('index.html');
   });
 
   it('should open and close Moderation plugin', () => {
-    openModerationPlugin();
-    cy.get('[data-testid="moderationRoot"]').should('exist');
-    closeModerationPlugin();
-    cy.get('[data-testid="moderationRoot"]').should('not.exist');
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      cy.get('[data-testid="moderationRoot"]').should('exist');
+      closeModerationPlugin();
+      cy.get('[data-testid="moderationRoot"]').should('not.exist');
+    });
   });
 
   it('should not allow clicking on report button', () => {
-    openModerationPlugin();
-    cy.get('[data-testid="submitButton"]').should('have.attr', 'aria-disabled', 'true');
-    closeModerationPlugin();
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      cy.get('[data-testid="submitButton"]').should('have.attr', 'aria-disabled', 'true');
+      closeModerationPlugin();
+    });
   });
 
   it('should validate labels of popover menu items', () => {
-    openModerationPlugin();
-    openPopoverMenu();
-    for (let index = 0; index < moderateItems.length; index++) {
-      const itemLabel = moderateItems[index].label;
-      cy.get('[data-testid="popoverMenu"]').contains(itemLabel).should('exist');
-    }
-    closeModerationPlugin();
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      openPopoverMenu();
+      for (let index = 0; index < moderateItems.length; index++) {
+        const itemLabel = moderateItems[index].label;
+        cy.get('[data-testid="popoverMenu"]').contains(itemLabel).should('exist');
+      }
+      closeModerationPlugin();
+    });
   });
 
   it('should allow clicking on report button', () => {
-    openModerationPlugin();
-    cy.get('[data-testid="submitButton"]').should('have.attr', 'aria-disabled', 'true');
-    openAndSelectItemFromDropdown(moderateItems[0].label);
-    cy.get('[data-testid="submitButton"]').should('have.attr', 'aria-disabled', 'false');
-    closeModerationPlugin();
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      cy.get('[data-testid="submitButton"]').should('have.attr', 'aria-disabled', 'true');
+      openAndSelectItemFromDropdown(moderateItems[0].label);
+      cy.get('[data-testid="submitButton"]').should('have.attr', 'aria-disabled', 'false');
+      closeModerationPlugin();
+    });
   });
 
   it('should submit a report without a comment', () => {
-    openModerationPlugin();
-    openAndSelectItemFromDropdown(moderateItems[0].label);
-    reportAndVerifyRequest(moderateItems[0].id);
-    // verify overlay was closed
-    cy.get('[data-testid="moderationRoot"]').should('not.exist');
-    // verify toast appears
-    cy.get('.playkit-interactive-area').contains('The report was sent successfully').should('exist');
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      openAndSelectItemFromDropdown(moderateItems[0].label);
+      reportAndVerifyRequest(moderateItems[0].id);
+      // verify overlay was closed
+      cy.get('[data-testid="moderationRoot"]').should('not.exist');
+      // verify toast appears
+      cy.get('.playkit-interactive-area').contains('The report was sent successfully').should('exist');
+    });
   });
 
   it('should submit a report with a comment', () => {
     const comment = 'Writing a comment to report on this video';
-    openModerationPlugin();
-    openAndSelectItemFromDropdown(moderateItems[2].label);
-    addComment(comment);
-    reportAndVerifyRequest( moderateItems[2].id, comment);
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      openAndSelectItemFromDropdown(moderateItems[2].label);
+      addComment(comment);
+      reportAndVerifyRequest(moderateItems[2].id, comment);
+    });
   });
 
   it('should reach textarea limit of 500 chars', () => {
@@ -88,14 +112,17 @@ describe('Moderation plugin', () => {
     for (let i = 0; i < 510; i++) {
       longText += 'a';
     }
-    openModerationPlugin();
-    addComment(longText);
-    cy.get('textarea').should($div => {
-      expect($div[0].textLength).to.eq(500);
+    mockKalturaBe();
+    loadPlayer().then(() => {
+      openModerationPlugin();
+      addComment(longText);
+      cy.get('textarea').should($div => {
+        expect($div[0].textLength).to.eq(500);
+      });
+      cy.get('[data-testid="characterCounter"]').should($div => {
+        expect($div[0].textContent).to.eq('500/500');
+      });
+      closeModerationPlugin();
     });
-    cy.get('[data-testid="characterCounter"]').should($div => {
-      expect($div[0].textContent).to.eq('500/500');
-    });
-    closeModerationPlugin();
   });
 });
--- cypress/e2e/utils.ts
+++ cypress/e2e/utils.ts
@@ -1,45 +1,97 @@
 export const openModerationPlugin = () => {
-    cy.get('.playkit-pre-playback-play-button').click({force: true});
-    cy.get('[data-testid="moderationPluginButton"]').click({force: true});
-}
+  cy.get('[data-testid="moderationPluginButton"]').click({force: true});
+};
 
 export const closeModerationPlugin = () => {
-    cy.get('.playkit-close-overlay').click({force: true});
-}
+  cy.get('.playkit-close-overlay').click({force: true});
+};
 
 export const openPopoverMenu = () => {
-    cy.get('[data-testid="selectButton"').click({force: true});
-    cy.get('[data-testid="popoverMenu"]').should('exist');
-}
+  cy.get('[data-testid="selectButton"').click({force: true});
+  cy.get('[data-testid="popoverMenu"]').should('exist');
+};
 
 export const openAndSelectItemFromDropdown = (text: string) => {
-    // open popover dropdown
-    openPopoverMenu();
+  // open popover dropdown
+  openPopoverMenu();
 
-    // choose an item from popover menu
-    cy.get('[data-testid="popoverMenu"]').contains(text).click({force: true});
-    cy.get('[data-testid="popoverMenu"]').should('not.exist');
+  // choose an item from popover menu
+  cy.get('[data-testid="popoverMenu"]').contains(text).click({force: true});
+  cy.get('[data-testid="popoverMenu"]').should('not.exist');
 
-    cy.get('[data-testid="moderationRoot"]').contains(text).should('exist');
-}
+  cy.get('[data-testid="moderationRoot"]').contains(text).should('exist');
+};
 
 export const addComment = (comment: string) => {
-    cy.get('textarea').type(comment, {force: true});
-}
+  cy.get('textarea').type(comment, {force: true});
+};
 
 export const reportAndVerifyRequest = (flagType: number, expectedComment?: string) => {
-    cy.intercept('POST', 'http://mock-api/service/multirequest').as('submit');
-    cy.get('[data-testid="submitButton"]').click({force: true});
-    cy.get('@submit').its('request.body[2]').should('deep.equal', {
-            service: "baseentry",
-            action: "flag",
-            moderationFlag: {
-                comments: expectedComment || "",
-                flagType: flagType,
-                flaggedEntryId: "0_wifqaipd",
-                objectType: "KalturaModerationFlag"
-            },
-            ks: "{1:result:ks}"
+  cy.get('[data-testid="submitButton"]').click({force: true});
+  cy.wait('@submit')
+    .its('request.body[2]')
+    .should('deep.equal', {
+      service: 'baseentry',
+      action: 'flag',
+      moderationFlag: {
+        comments: expectedComment || '',
+        flagType: flagType,
+        flaggedEntryId: '0_wifqaipd',
+        objectType: 'KalturaModerationFlag'
+      },
+      ks: '{1:result:ks}'
+    });
+};
+
+const getPlayer = () => {
+  // @ts-ignore
+  return cy.window().then($win => $win.KalturaPlayer.getPlayers()['player-placeholder']);
+};
+
+const preparePage = (pluginConf = {}, playbackConf = {}) => {
+  cy.visit('index.html');
+  return cy.window().then(win => {
+    try {
+      // @ts-ignore
+      var kalturaPlayer = win.KalturaPlayer.setup({
+        targetId: 'player-placeholder',
+        provider: {
+          partnerId: -1,
+          env: {
+            cdnUrl: 'http://mock-cdn',
+            serviceUrl: 'http://mock-api'
+          }
+        },
+        playback: {muted: true, autoplay: true, ...playbackConf},
+        plugins: {
+          'playkit-js-moderation': pluginConf
         }
-    );
-}
+      });
+      return kalturaPlayer.loadMedia({entryId: '0_wifqaipd'});
+    } catch (e: any) {
+      return Promise.reject(e.message);
+    }
+  });
+};
+
+export const loadPlayer = (pluginConf = {}, playbackConf = {}) => {
+  return preparePage(pluginConf, playbackConf).then(() => getPlayer().then(kalturaPlayer => kalturaPlayer.ready().then(() => kalturaPlayer)));
+};
+
+const checkRequest = (reqBody: any, service: string, action: string) => {
+  return reqBody?.service === service && reqBody?.action === action;
+};
+
+export const mockKalturaBe = (entryFixture = 'vod-entry.json') => {
+  cy.intercept('http://mock-api/service/multirequest', req => {
+    if (checkRequest(req.body[2], 'baseEntry', 'list')) {
+      return req.reply({fixture: entryFixture});
+    }
+    if (checkRequest(req.body[2], 'baseentry', 'flag')) {
+      req.alias = 'submit';
+      return req.reply({fixture: 'report.json'});
+    }
+  });
+  cy.intercept('GET', '**/ks/123', {fixture: 'thumb-asset.jpeg'}).as('getSlides');
+  cy.intercept('GET', '**/vid_sec/*', {fixture: 'thumb-asset.jpeg'}).as('getChapters');
+};
